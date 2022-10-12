caa6017ba03dc309db73be93892d83f461060313
ooze <grigorianna@gmail.com>
2022-10-12 10:35:10 +0400

    Add Еxample 008 FriendsDog

---
+++ Exsmple008_FriendsDog/Program.cs
@@ -0,0 +1,27 @@
+﻿int count = 0;
+int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+bool friend2 = true;
+int time;
+
+while(distance > 10)
+{
+    if(friend2 == false)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend2 = true;
+    }
+    else
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend2 = false;
+    }
+
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count = count + 1;
+}
+
+Console.Write("count = ");
+Console.WriteLine(count);
