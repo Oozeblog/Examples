edef37c17abdeba2d2f22eacc2dd348f6de93cd3
Johann <tmkcorp@yandex.ru>
2022-10-12 11:48:17 +0300

    change string to var

--- Еxample005_ConditionIfElse/Program.cs
+++ Еxample005_ConditionIfElse/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Write("Введите имя пользователя ");
-string username = Console.ReadLine();
+var username = Console.ReadLine();
 if(username.ToLower() == "маша")
 {
     Console.WriteLine("Ура! Это же Маша!");
