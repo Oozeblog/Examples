ea5415cc4bf5f4ce91454a3a508cec24fbb7c14f
ooze <grigorianna@gmail.com>
2022-10-15 15:35:18 +0400

    Update Example011 ArroyLibrary

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -22,8 +22,30 @@ void PrintArray(int[] col)
     }
 }
 
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
 
 int [] array = new int [10];
 
 FillArray(array);
 PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 45);
+Console.WriteLine(pos);
