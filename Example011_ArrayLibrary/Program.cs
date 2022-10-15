cd512b991b55a1c8795a9fd08e2766ac56de961d
ooze <grigorianna@gmail.com>
2022-10-15 15:23:45 +0400

    Added Example011 ArroyLibrary

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,29 @@
+﻿//void - метод который не возвращает данные. Не нужно использовать оперетор  retorn
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+
+int [] array = new int [10];
+
+FillArray(array);
+PrintArray(array);
