a53187f67b5ed42682d625f931b165be6f8685c9
ooze <grigorianna@gmail.com>
2022-10-22 13:52:39 +0400

    Update Example012 Methods

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -53,3 +53,27 @@ string Method4(int count, string text)
 string res = Method4(10, "asdf");
 Console.WriteLine(res);
 */
+// Цикл for
+/*
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for(int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(10, "asdf");
+Console.WriteLine(res);
+*/
+
+//Цикл в цикле. Вывод таблицы умножения на экран
+for(int i =2; i <= 10; i++)
+{
+    for(int j =2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i*j}");
+    }
+    Console.WriteLine();
+}
